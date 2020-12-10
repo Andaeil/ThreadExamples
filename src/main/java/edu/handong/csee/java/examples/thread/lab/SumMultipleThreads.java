1f77a21554cfbeb6b0bacb14ef881b4b637d985d
Andaeil <71754742+Andaeil@users.noreply.github.com>
2020-12-10 14:41:56 +0900

    done Lab 12

--- src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
+++ src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
@@ -21,21 +21,48 @@ public class SumMultipleThreads {
 		 * sumRunners.add(new SumMultipleThreads(7000001,8000000));
 		 * sumRunners.add(new SumMultipleThreads(8000001,9000000));
 		 * sumRunners.add(new SumMultipleThreads(9000001,10000000));*/
+//		for(long i=0; i<to/1000000; i++) {
+//			SumRunner currentRunner = new SumRunner((i*1000000)+1, (i+1)*1000000);
+//			sumRunners.add(currentRunner);
+//			
+//			Thread thread = new Thread(currentRunner);
+//			thread.start();
+//			threadsForSubSum.add(thread);
+//			System.out.println("Thread-" + i + " started!");
+//		}
+//
+//		long grandTotal = 0;
+//		for(SumRunner runner:sumRunners) {
+//			grandTotal += runner.totalSum;
+//		}
+		long grandTotal = 0;
+		
+		for(long i=0; i<to/1000000; i++) {
+			
+			System.out.println("Thread-" + i + " started!");
+
+		}
+		
 		for(long i=0; i<to/1000000; i++) {
 			SumRunner currentRunner = new SumRunner((i*1000000)+1, (i+1)*1000000);
 			sumRunners.add(currentRunner);
 			
 			Thread thread = new Thread(currentRunner);
 			thread.start();
+			
 			threadsForSubSum.add(thread);
-			System.out.println("Thread-" + i + " started!");
+			
+			try {
+				thread.join();
+			}catch(Exception e){
+				
+			}
 		}
 
-		long grandTotal = 0;
 		for(SumRunner runner:sumRunners) {
 			grandTotal += runner.totalSum;
 		}
-
+		
 		System.out.println("Grand Total = " + grandTotal);
 	}
 
